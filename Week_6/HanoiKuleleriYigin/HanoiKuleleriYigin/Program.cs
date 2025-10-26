d93844f1c324ba0cbe9e681fa8fe6ee802b99bb8
FKAV64 <fomavalerio@gmail.com>
2025-10-26 20:49:38 +0300

    Week_6 assignment

---
+++ Week_6/HanoiKuleleriYigin/HanoiKuleleriYigin/Program.cs
@@ -0,0 +1,24 @@
+﻿class hanoiKuleriYigin
+{
+    public static void Hanoi(int n, char start, char end, char auxilary)
+    {
+        if(n == 1)
+        {
+            Console.WriteLine($"{start} --> {end}");
+            return;
+        }
+        else
+        {
+            Hanoi(n - 1, start, auxilary, end);
+            Console.WriteLine($"{start} --> {end}");
+            Hanoi(n - 1, auxilary, end, start);
+        }
+    }
+    static void Main()
+    {
+        Console.Write("n degerini giriniz: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Aşağıda A kulesinden C kulesine geçmek için gereken tüm adımlar yer almaktadır");
+        Hanoi(n, 'A', 'C', 'B');
+    }
+}
